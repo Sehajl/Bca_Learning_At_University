9594b69059b23e1ab6db22566b282e2346f94f66
Sehajl <128030859+Sehajl@users.noreply.github.com>
2023-03-19 22:17:06 +0530

    Update Assignment1.c

--- Assignment1.c
+++ Assignment1.c
@@ -2,6 +2,26 @@
 
 int main()
 {
-  //add your code here
+ #include<stdio.h>
+int main()
+{
+	int i,posi=0,nega=0,n,k;
+	printf("Enter a number of inputs: ");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&k);
+		if(k>0)
+		{
+			posi=posi+1;
+		}
+		else
+		{
+			nega=nega+1;
+		}
+	}
+	printf("Number of positive number are: %d\n",posi);
+	printf("Number of negative number are: %d",nega);
+}
   return 0;
 }
