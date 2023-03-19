3cd706e6ab246ffdc49b8647e49e5bae64e462c2
Sehajl <128030859+Sehajl@users.noreply.github.com>
2023-03-19 22:29:39 +0530

    Update Assignment3.c

--- Assignment3.c
+++ Assignment3.c
@@ -3,5 +3,19 @@
 
 int main()
 {
-  return 0;
+ #include<stdio.h>
+  void main()
+  {
+    int r,i,bin=0,pr=1;
+	printf("Enter a number in decimal: ");
+	scanf("%d",&i);
+	while(i!=0)
+	{
+		r=i%2;
+		bin=bin+(pr*r);
+		pr=pr*10;
+		i=i/2;
+	}
+	printf("Binary of the number is: %d",bin);
+}*/
 }
